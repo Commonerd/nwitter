9850af25061b7d0b3a8485367b50261cb0859c0c
commonerd <cornflake127@naver.com>
2022-11-10 08:07:41 +0900

    '221110_ReactServerConfig'

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAlidrpEXn5wyBx3omBEuhcAAbhNdJLvZc",
+    authDomain: "nwitter-1a7f1.firebaseapp.com",
+    projectId: "nwitter-1a7f1",
+    storageBucket: "nwitter-1a7f1.appspot.com",
+    messagingSenderId: "294685755695",
+    appId: "1:294685755695:web:ae9942717c6ed4c28b39da"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';mport './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import firebase from "./firebase";
+console.log(firebase);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
