3b714d4b35d8694dd21e478406bd09fe20dc9f51
commonerd <cornflake127@naver.com>
2023-02-09 22:28:48 +0900

    '230209'

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,18 @@
+import { dbService } from "fbase";
 import { useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        await dbService.collection("nweets").add({
+            text: nweet,
+            createdAt: Date.now(),
+        });
+        setNweet("");
     };
+
 
     const onChange = (event) => {
         event.preventDefault();
